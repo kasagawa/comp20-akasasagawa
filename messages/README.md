08202b89613f927c3343d9ce7aa3df6e8483736c
Anna Kasagawa <anna.kasagawa@gmail.com>
2016-03-10 23:23:38 -0500

    Lab 6 updated

---
+++ messages/README.md
@@ -0,0 +1,13 @@
+Anna Kasagawa
+3/10/16
+COMP20 LAB 6
+
+
+What works: Opens,parses, and output message.
+Collaborated with: Joe Kamibeppu
+Hours Spent: 1.5 hours
+
+Is it possible to request the data from a different origin from using XMLHttpRequest? 
+No, it is not possible to request the data from a different origin. The data that is being parsed must
+be locally available, and javascript needs a server to run. Python is the server, but just opening
+the file on chrome doesn't run anything, so the request and response doesn't happen.
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,19 @@
-// Your JavaScript goes here...
+function parse(){
+
+	var jsondata = "data.json";
+	var request = new XMLHttpRequest();
+
+	request.open("GET", jsondata, true);
+
+	request.onreadystatechange = function() {
+		if (request.status == 200 & request.readyState == 4) {
+			var messages = JSON.parse(request.responseText);
+			
+			document.getElementById("messages").innerHTML = 
+			messages[0].content + " " + messages[0].username + "<br>" +
+			messages[1].content + " " + messages[1].username + "<br>";
+		}
+	}
+	request.send();
+}
+
